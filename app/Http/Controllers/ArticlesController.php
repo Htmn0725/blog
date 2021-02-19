0a9586948ad9aa448e5b18076a2169338f07bd27
hitomi sato <l.sato@enjoyworks.jp>
2021-02-20 00:16:56 +0900

    first commit

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use App\http\Resources\Article as ArticleResource;
+use Illuminate\Http\Request;
+
+class ArticlesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Get articles
+        //$articles = Article::paginate(15);
+        $articles = Article::orderBy('created_at', 'desc')->paginate(5);
+
+        // Return collection of articles as a resource
+        return ArticleResource::collection($articles);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $article = $request->isMethod('put') ? Article::findOrFail($request->article_id) : new Article;
+
+        $article->id = $request->article_id;
+        $article->title = $request->title;
+        $article->body = $request->body;
+
+        if ($article->save()) {
+            return new ArticleResource($article);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // Get article
+        $article = Article::findOrFail($id);
+
+        // Return single article as a resource
+        return new ArticleResource($article);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $article = $request->isMethod('put') ? Article::findOrFail($request->article_id) : new Article;
+
+        $article->id = $request->article_id;
+        $article->title = $request->title;
+        $article->body = $request->body;
+
+        if ($article->save()) {
+            return new ArticleResource($article);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // Get article
+        $article = Article::findOrFail($id);
+
+        if ($article->delete()) {
+            return new ArticleResource($article);
+        }
+    }
+}
---
+++ app/Http/Resources/Article.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Article extends JsonResource
+{
+    /**
+     * The "data" wrapper that should be applied.
+     *
+     * @var string
+     */
+    //public static $wrap = 'article';
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+
+        // return  [
+        //     'id' => $this->id,
+        //     'title' => $this->title,
+        //     'body' => $this->body,
+        // ];
+    }
+
+    // public function with($request)
+    // {
+    //     // return [
+    //     //     'version' => '1.0.0',
+    //     //     'author_url' => url('http://google.com')
+    //     // ];
+    // }
+}
